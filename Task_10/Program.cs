eacb449a1c4eca2e4a30c310649e7cca8b9fb02a
Vladislav <SimashVlad@yandex.ru>
2022-05-15 23:07:12 +0500

    Task_10

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+// int number = new Random().Next(100, 1000); Console.WriteLine(number); // для проверки
+
+int num1 = number / 10;
+int result = num1 % 10;
+
+Console.WriteLine(result);
